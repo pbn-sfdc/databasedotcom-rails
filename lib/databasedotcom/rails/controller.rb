e760c09cd08acc47aa3bb67a622073b4a3c30d4e
Danny Burkes <dburkes@netable.com>
2011-08-24 00:48:06 -0700

    use databasedotcom gem, now that it's published

--- lib/databasedotcom/rails/controller.rb
+++ lib/databasedotcom/rails/controller.rb
@@ -4,13 +4,23 @@ module Databasedotcom
       module ClassMethods
         def dbdc_client
           unless @dbdc_client
-            config = YAML.load_file(File.join(::Rails.root, 'config', 'databasedotcom.yml'))
-            username = config["username"]
-            password = config["password"]
-            @dbdc_client = Databasedotcom::Client.new(config)
+            if ENV['DATABASE_COM_URL']
+              @dbdc_client = Databasedotcom::Client.new
+            else
+              config = YAML.load_file(File.join(::Rails.root, 'config', 'databasedotcom.yml'))
+              @dbdc_client = Databasedotcom::Client.new(config)
+            end
+            @dbdc_client.debug = true
+            password = @dbdc_client.password
+            username = @dbdc_client.username
+            if ENV['DATABASE_COM_SECURITY_TOKEN']
+              stoken = ENV['DATABASE_COM_SECURITY_TOKEN']
+              pwd_stoken = "#{@dbdc_client.password}#{stoken}"
+              password = pwd_stoken
+            end
+            puts "Using [#{username}][#{password}] to authenticate"
             @dbdc_client.authenticate(:username => username, :password => password)
           end
-
           @dbdc_client
         end
         
